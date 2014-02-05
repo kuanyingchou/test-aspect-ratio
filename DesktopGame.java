0fcd2b1b4a9e5de96202346cb731061d54b4b494
ken <kuanyingchou@gmail.com>
2014-02-05 18:18:36 +0800

    remove unused files

--- DesktopGame.java
+++
@@ -1,8 +0,0 @@
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-
-public class DesktopGame {
-    public static void main (String[] args) {
-        LwjglApplication app = new LwjglApplication(
-                new Game(), "test-aspect-ratio", 1600, 600, false);
-    }
-}
