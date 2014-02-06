03602fa6f24628b9355c3f84192040282b13c3f9
ken <kuanyingchou@gmail.com>
2014-02-06 16:15:30 +0800

    add build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+javac *.java
+java Main 1420 964 1420x964.png
+java Main 1920 1080 1920x1080.png
+java Main 2048 1280 2048x1280.png
+java Main 2048 1536 2048x1536.png
+java Main 2048 1462 2048x1462.png
