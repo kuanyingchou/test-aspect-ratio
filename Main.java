07a4db52a6c323a2467cf439c364b36c8f902a4c
ken <kuanyingchou@gmail.com>
2014-02-06 16:15:18 +0800

    add command options and other stuff

--- Main.java
+++ Main.java
@@ -4,31 +4,44 @@ import javax.imageio.*;
 import java.io.*;
 
 class Main {
-    //private static int width = 1420, height = 964; 
-    //private static int width = 2048, height = 1536; 
-    private static int width = 2048, height = 1280;
-    public static String filename = "ratios2048-1.6.png";
+    private static int width;
+    private static int height;
+    private static boolean enableText = false;
 
-    public static boolean enableText = true;
     private static Color[] colors = new Color[] {
-        /*
-        Color.white,
-        Color.red,
-        Color.green,
-        Color.yellow,
-        Color.cyan,
-        Color.magenta,
-        Color.orange,
-        */
         new Color(105,210,231),
         new Color(167,219,216),
         new Color(224,228,204),
         new Color(243,134,48),
-        new Color(250,105,0)
+        new Color(250,105,0),
+        new Color(236,208,120),
+        new Color(217,91,67),
+        new Color(192,41,66),
+        new Color(250,208,137),
+        new Color(83,119,122)
     };
     private static int colorIndex = 0;
 
     public static void main(String[] args) throws IOException {
+        if(args.length < 3) {
+            printUsage();
+            return;
+        }
+        if(args[2] == null || args[2].equals("")) {
+            System.out.println(
+                    "Please specify an output filename. Exiting now.");
+            return;
+        }
+        for(int i=0; i<2; i++) {
+            if(args[i] == null || args[i].equals("")) {
+                printUsage();
+                return;
+            }
+        }
+
+        width = Integer.valueOf(args[0]);
+        height = Integer.valueOf(args[1]);
+
         final BufferedImage img = new BufferedImage(
                 width, height, BufferedImage.TYPE_INT_RGB);
         final Graphics g = img.getGraphics();
@@ -37,62 +50,76 @@ class Main {
         g.setColor(Color.black);
         g.fillRect(0, 0, width, height);
 
-        //test(g);
+        //testDrawRect(g);
         //drawRects(g);
         drawRatios(g);
 
-        ImageIO.write(img, "png", new File(filename));
+        ImageIO.write(img, "png", new File(args[2]));
     }
 
-    private static void test(Graphics g) {
+    private static void printUsage() {
+        System.out.println("Usage: java Main 2048 1536 2048x1536.png");
+    }
+
+    private static void testDrawRect(Graphics g) {
         width = height = 4;
         drawRect(g, 2, 2);
     }
     private static void drawRects(Graphics g) {
-        //drawRect(g, 2048, 1536);
-        //drawRect(g, 1920, 1200);
-        //drawRect(g, 1920, 1080);
+        drawRect(g, 2048, 1536, "iPad Air");
+        drawRect(g, 1920, 1200, "Nexus 7 2013");
+        drawRect(g, 1920, 1080, "Galaxy S4");
         drawRect(g, 1420, 964);
-        drawRect(g, 1280, 800);
-        //drawRect(g, 1280, 720);
-        //drawRect(g, 1024, 768);
-        drawRect(g, 1136, 640);
-        drawRect(g, 960, 640);
-        //drawRect(g, 800, 480);
-        //drawRect(g, 480, 320);
-        //drawRect(g, 1420, 964);
+        drawRect(g, 1280, 800, "Nexus 7 2012");
+        drawRect(g, 1280, 720, "Galaxy Note II");
+        drawRect(g, 1024, 768);
+        drawRect(g, 1136, 640, "iPhone 5/5s");
+        drawRect(g, 960, 640, "iPhone 4/4s");
+        drawRect(g, 800, 480, "HTC Desire");
+        drawRect(g, 480, 320, "iPhone 3G/3GS");
+        drawRect(g, 5120, 2160, "4K"); 
+        drawRect(g, 4096, 2160, "DCI 4K");
     }
     private static void drawRatios(Graphics g) {
         drawRatio(g, 2048, 1536, "iPad Air");
-        //drawRatio(g, 1920, 1200);
-        ////drawRatio(g, 1920, 1080, "Galaxy S4");
-        drawRatio(g, 1280, 800, "Kizipad I");
-        //drawRatio(g, 1280, 720);
-        //drawRatio(g, 1024, 768);
-        drawRatio(g, 1136, 640);
-        ////drawRatio(g, 960, 640, "iPhone 4s");
-        ////drawRatio(g, 800, 480);
-        ////drawRatio(g, 1024, 600);
-        //drawRatio(g, 1, 1);
-        ////drawRatio(g, 5120, 2160, "4K"); 
-        ////drawRatio(g, 4096, 2160, "DCI 4K");
+        drawRatio(g, 1920, 1200, "Nexus 7 2013");
+        drawRatio(g, 1920, 1080, "Galaxy S4");
         drawRatio(g, 1420, 964);
-        //drawRatio(g, 480, 320);
+        drawRatio(g, 1280, 800, "Nexus 7 2012");
+        drawRatio(g, 1280, 720, "Galaxy Note II");
+        drawRatio(g, 1024, 768);
+        drawRatio(g, 1136, 640, "iPhone 5/5s");
+        drawRatio(g, 960, 640, "iPhone 4/4s");
+        drawRatio(g, 800, 480, "HTC Desire");
+        drawRatio(g, 480, 320, "iPhone 3G/3GS");
+        //drawRatio(g, 5120, 2160, "4K"); 
+        //drawRatio(g, 4096, 2160, "DCI 4K");
     }
 
     private static void drawRect(Graphics g, int w, int h) {
+        drawRect(g, w, h, "");
+    }
+    private static void drawRect(Graphics g, int w, int h, String comment) {
         int x = (width - w) / 2;
         int y = (height - h) / 2;
-        g.setColor(colors[colorIndex++ % colors.length]);
+        final Color c = colors[colorIndex++ % colors.length];
+        g.setColor(new Color(c.getRed(), c.getGreen(), c.getBlue(), 16));
+        g.fillRect(x, y, w-1, h-1);
+        g.setColor(c);
         g.drawRect(x, y, w-1, h-1);
-        g.drawString(w+"x"+h, x+2, y+14);
+        final String addon = (enableText && !isNullOrEmpty(comment))?
+                " ("+comment+") ":"";
+        g.drawString(w+"x"+h+addon, x+2, y+14);
     }
+
     private static void drawRatio(Graphics g, int w, int h) {
         drawRatio(g, w, h, "");
     }
-    private static void drawRatio(Graphics g, int w, int h, String comment) {
+    private static void drawRatio(
+            Graphics g, int w, int h, String comment) {
         float screen = (float)width / height;
-        System.out.println(String.format("drawing %dx%d: %f", w, h, (float)w/h));
+        System.out.println(
+                String.format("drawing %dx%d: %f", w, h, (float)w/h));
         float ratio = (float)w/h;
         g.setColor(colors[colorIndex++ % colors.length]);
         int nx, ny, nw, nh;
@@ -115,7 +142,19 @@ class Main {
         g.drawRect(nx, ny, nw - 1, nh - 1);
         if(enableText) {
             //g.setColor(Color.black);
-            g.drawString(w+" x "+h+" - "+ratio+" ("+comment+")", nx + 2, ny + 14);
+            if(isNullOrEmpty(comment)) {
+                g.drawString(
+                    w+" x "+h+" - "+ratio, nx + 2, ny + 14);
+            } else {
+                g.drawString(
+                    w+" x "+h+" - "+ratio+" ("+comment+")", nx + 2, ny + 14);
+            }
         }
+    }
+
+    public static boolean isNullOrEmpty(String s) {
+        if(s == null) return true;
+        if(s.equals("")) return true;
+        return false;
     }
 }
